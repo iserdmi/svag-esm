618c860e9e4fe9967dad6df9340cc3218aa45377
Sergei Dmitriev <iserdmi@gmail.com>
2024-05-14 08:13:53 +0400

    Small fix

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+import svagJestConfigBase from 'svag-jest/configs/base.js'
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  ...svagJestConfigBase,
+}
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { argv } from 'process'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { createRequire } from 'module'
+
+//Thanks: https://gist.github.com/khalidx/1c670478427cc0691bda00a80208c8cc
+
+/**
+ * This is an ESM replacement for `__filename`.
+ *
+ * Use it like this: `__filename(import.meta)`.
+ */
+export const get__filename = (meta: ImportMeta): string => fileURLToPath(meta.url)
+
+/**
+ * This is an ESM replacement for `__dirname`.
+ *
+ * Use it like this: `__dirname(import.meta)`.
+ */
+export const get__dirname = (meta: ImportMeta): string => dirname(get__filename(meta))
+
+/**
+ * Indicates that the script was run directly.
+ * This is an ESM replacement for `require.main === module`.
+ *
+ * Use it like this: `isMain(import.meta)`.
+ */
+export const isMain = (meta: ImportMeta): boolean => {
+  if (!meta || !argv[1]) return false
+  const require = createRequire(meta.url)
+  const scriptPath = require.resolve(argv[1])
+  const modulePath = get__filename(meta)
+  return scriptPath === modulePath
+}
